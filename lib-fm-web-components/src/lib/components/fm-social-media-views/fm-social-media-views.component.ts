82461151a48f1c8b5e7704afb827da92cfc390e3
Clarerodev <clarerodri.dev@gmail.com>
2023-11-28 21:54:48 -0500

    feat: Add theme to social-media views component

--- lib-fm-web-components/src/lib/components/fm-social-media-views/fm-social-media-views.component.ts
+++ lib-fm-web-components/src/lib/components/fm-social-media-views/fm-social-media-views.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'fm-social-media-views',
-  standalone: true,
-  imports: [CommonModule],
+  selector: 'fm-social-media-views-lib',
   templateUrl: './fm-social-media-views.component.html',
   styleUrls: ['./fm-social-media-views.component.scss'],
 })
-export class FmSocialMediaViewsComponent {}
+export class FmSocialMediaViewsComponent {
+  @Input() followerData: any = null;
+  @Input() theme: string = 'light';
+}
--- lib-fm-web-components/src/lib/fm-web-components.module.ts
+++ lib-fm-web-components/src/lib/fm-web-components.module.ts
@@ -3,12 +3,17 @@ import { APP_INITIALIZER, CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core
 import { defineCustomElements } from '@clarerodev/fm-web-components/dist/loader';
 import { FmSocialMediaFollowersComponent } from "./components/fm-social-media-followers/fm-social-media-followers.component";
 import { FmSwitchComponent } from "./components/fm-switch/fm-switch.component";
+import { FmSocialMediaViewsComponent } from "./components/fm-social-media-views/fm-social-media-views.component";
 
 defineCustomElements(window)
 @NgModule({
-    declarations: [FmSwitchComponent, FmSocialMediaFollowersComponent],
+    declarations: [FmSwitchComponent,
+                FmSocialMediaFollowersComponent,
+                FmSocialMediaViewsComponent],
     imports: [CommonModule],
-    exports: [FmSwitchComponent, FmSocialMediaFollowersComponent],
+    exports: [FmSwitchComponent,
+        FmSocialMediaFollowersComponent,
+        FmSocialMediaViewsComponent],
     providers: [
         {
             provide: APP_INITIALIZER,
