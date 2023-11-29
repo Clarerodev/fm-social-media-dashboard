7aa36a68a804250e662fbb70e6308cbd98ff11ee
Clarerodev <clarerodri.dev@gmail.com>
2023-11-28 21:55:08 -0500

    feat: Add view-dashboard component

--- apps/fm-social-media/src/components/follower-views-dashboard/follower-views-dashboard.component.ts
+++ apps/fm-social-media/src/components/follower-views-dashboard/follower-views-dashboard.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FmWebComponentsModule } from '@fm-social-media-monorepo/lib-fm-web-components';
 
 @Component({
-  selector: 'fm-social-media-monorepo-follower-views-dashboard',
+  selector: 'fm-views-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FmWebComponentsModule],
   templateUrl: './follower-views-dashboard.component.html',
   styleUrls: ['./follower-views-dashboard.component.scss'],
 })
-export class FollowerViewsDashboardComponent {}
+export class FollowerViewsDashboardComponent {
+  @Input() theme: string = 'light';
+  public viewData: any;
+
+  constructor() {
+    this.generateData();
+  }
+
+  private generateData() {
+    this.viewData = [
+      { type: 'facebook', likesCount: '87', likesByDay: '3' },
+      { type: 'facebook', likesCount: '52', likesByDay: '-2' },
+      { type: 'instagram', likesCount: '5462', likesByDay: '2257' },
+      { type: 'instagram', likesCount: '52k', likesByDay: '1375' },
+      { type: 'twitter', likesCount: '117', likesByDay: '303' },
+      { type: 'twitter', likesCount: '507', likesByDay: '553' },
+      { type: 'youtube', likesCount: '107', likesByDay: '-19' },
+      { type: 'youtube', likesCount: '1407', likesByDay: '-12' },
+    ]
+  }
+}
--- apps/fm-social-media/src/components/followers/followers.component.ts
+++ apps/fm-social-media/src/components/followers/followers.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FollowerDashboardComponent } from '../follower-dashboard/follower-dashboard.component';
 import { FollowerHeaderComponent } from '../follower-header/follower-header.component';
+import { FollowerViewsDashboardComponent } from '../follower-views-dashboard/follower-views-dashboard.component';
 
 @Component({
   selector: 'fm-followers',
   standalone: true,
-  imports: [CommonModule, FollowerDashboardComponent, FollowerHeaderComponent],
+  imports: [CommonModule, FollowerDashboardComponent, FollowerHeaderComponent, FollowerViewsDashboardComponent],
   templateUrl: './followers.component.html',
   styleUrls: ['./followers.component.scss'],
 })
